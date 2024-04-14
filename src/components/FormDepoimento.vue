4631dca37552405208be45a820fa3b4262272a31
Bruno-Costa-fig <brunocosta.fig@gmail.com>
2024-04-14 16:53:38 -0300

    feat: informações importantes

--- src/components/FormDepoimento.vue
+++ src/components/FormDepoimento.vue
@@ -1,18 +1,26 @@
 <template>
   <form
     v-if="showForm"
-    class="bg-white shadow-md rounded px-8 py-4"
+    class="bg-white shadow-md rounded px-8 py-4 text-gray-700"
     @submit.prevent="handleEnviarDepoimento"
   >
-    <label class="block text-gray-700 text-sm font-bold mb-2"> Seu nome </label>
+    <h2 class="text-2xl">Deixe aqui seu depoimento:</h2>
+    <p class="mb-8 mt-2">
+      Os depoimentos são uma forma de validar a qualidade do meu trabalho e de
+      ajudar outras pessoas a decidirem se querem ou não trabalhar/estudar comigo. Se
+      você já foi meu aluno ou trabalhou comigo, deixe aqui seu feedback. Os
+      depoimentos após postados aparecerão na seção de feedbacks abaixo em <b>tempo
+      real.</b>
+    </p>
+    <label class="block  text-sm font-bold mb-2"> Seu nome </label>
     <input
       type="text"
       required
       v-model="depoimento.nomeAluno"
       placeholder="Digite seu nome"
-      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+      class="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
     />
-    <label class="block text-gray-700 text-sm font-bold mb-2"
+    <label class="block text-sm font-bold mb-2"
       >Sua profissão (opcional)</label
     >
     <input
@@ -20,9 +28,9 @@
       required
       v-model="depoimento.profissao"
       placeholder="Digite sua profissão"
-      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+      class="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
     />
-    <label class="block text-gray-700 text-sm font-bold mb-2">
+    <label class="block text-sm font-bold mb-2">
       Feedback (Máx. 500 caracteres)</label
     >
     <textarea
@@ -30,32 +38,51 @@
       :maxlength="500"
       placeholder="Seu feedback - máximo 500 caracteres"
       v-model="depoimento.depoimento"
-      class="resize shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+      class="resize shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
     />
-    <label class="block text-gray-700 text-sm font-bold mb-2"
+    <label class="block text-sm font-bold mb-2"
       >LinkedIn (opcional)</label
     >
     <input
       type="text"
       v-model="depoimento.linkLinkedin"
       placeholder="Link do seu perfil no linkedIn"
-      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+      class="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
     />
-    <label class="block text-gray-700 text-sm font-bold mb-2"
+    <label class="block text-sm font-bold mb-2"
       >GitHub (opcional)</label
     >
     <input
       type="text"
       v-model="depoimento.userGithub"
       placeholder="Username do seu perfil no GitHub"
-      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+      class="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
     />
-    <label class="block text-gray-700 text-sm font-bold mb-2">
+    <label class="block text-sm font-bold mb-2">
       Permite usar sua imagem do perfil do GitHub?
       <input class="ms-2" type="checkbox" v-model="depoimento.permiteFoto" />
       <span class="ms-2 text-gray-500 font-light"
         >Sua foto será exibida no card junto ao seu depoimento</span
       >
+    </label>
+    <h4 class="block font-bold">Observações</h4>
+    <label class="block text-sm mb-2">
+      O link do seu LinkedIn servirá apenas para direcionar outros usuários para
+      seu perfil, como forma de validação do depoimento e comprovação de
+      identidade, mas não será usado em momento algum para nenhuma outra
+      finalidade por parte da equipe da Bruno Costa Dev.
+    </label>
+    <label class="block text-sm mb-2">
+      O link do seu Github servirá apenas para buscar sua foto de perfil, caso
+      você permita, para exibição no card do depoimento. Nenhum outro dado será
+      coletado ou armazenado por parte da equipe da Bruno Costa Dev.
+    </label>
+    <label class="block text-sm mb-2">
+      Os depoimentos aqui contidos podem a qualquer momento serem removidos ou
+      reportados por usuários que se sintam ofendidos ou que achem que o
+      conteúdo é inadequado ou irreal. A equipe da Bruno Costa Dev se reserva o direito de
+      avaliar e remover depoimentos que não estejam de acordo com as políticas
+      de uso do site.
     </label>
 
     <div class="flex">
